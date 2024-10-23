fdb48105c469c4a2bcc89e11491b2ffd79b1ec82
Lilian Besson <lilian.besson@crans.org>
2024-10-23 19:38:06 +0200

    OCaml code (@external) and signature, of such C functions defined elsewhere (see previous commit).

--- targets/numworks/numworks.ml
+++ targets/numworks/numworks.ml
@@ -1,8 +1,17 @@
 (* Timing *)
 
-external delay: int -> unit = "caml_delay" [@@noalloc]
+external delay : int -> unit = "caml_delay" [@@noalloc]
+external delay_usec : int -> unit = "caml_delay_usec" [@@noalloc]
 external millis : unit -> int = "caml_millis" [@@noalloc]
 
 (* Printing *)
 
 external print_string : string -> unit = "caml_numworks_print_string" [@@noalloc]
+external print_newline : unit -> unit = "caml_numworks_print_newline" [@@noalloc]
+external print_endline : string -> unit = "caml_numworks_print_endline" [@@noalloc]
+external print_int : int -> unit = "caml_numworks_print_int" [@@noalloc]
+(* external print_char : char -> unit = "caml_numworks_print_char" [@@noalloc] *)
+(* external print_float : float -> unit = "caml_numworks_print_float" [@@noalloc] *)
+
+(* EADK library *)
+external display_draw_string : string -> int -> int -> unit = "caml_display_draw_string" [@@noalloc]
--- targets/numworks/numworks.mli
+++ targets/numworks/numworks.mli
@@ -1,8 +1,18 @@
 (* Timing *)
 
-val delay: int -> unit
-val millis: unit -> int
+val delay : int -> unit
+val delay_usec : int -> unit
+val millis : unit -> int
 
 (* Printing *)
 
 val print_string : string -> unit
+val print_newline : unit -> unit
+val print_endline : string -> unit
+val print_int : int -> unit
+(* val print_char : char -> unit *)
+(* val print_float : float -> unit *)
+
+(* EADK library *)
+val display_draw_string : string -> int -> int -> unit
+
