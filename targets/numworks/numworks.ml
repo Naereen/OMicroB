084c69b1f07a3620560f5a8ca7b3cc2a6805e266
Lilian Besson <lilian.besson@crans.org>
2024-10-23 22:42:36 +0200

    Starting to document a bit the Numworks OCaml module.

--- targets/numworks/numworks.ml
+++ targets/numworks/numworks.ml
@@ -1,17 +1,59 @@
-(* Timing *)
+(**********************************)
+(** Code of the Numworks library **)
+(**********************************)
 
+(**********)
+(* Timing *)
+(**********)
+
+(** delay ms : sleeps for this amount of milliseconds. *)
 external delay : int -> unit = "caml_delay" [@@noalloc]
+
+(** delay us : sleeps for this amount of microseconds. *)
 external delay_usec : int -> unit = "caml_delay_usec" [@@noalloc]
+
+(** millis () : gives the amount of milliseconds since launching the program/machine
+    (detail is not important, only difference between two millis() is useful). *)
 external millis : unit -> int = "caml_millis" [@@noalloc]
 
+(************)
 (* Printing *)
+(************)
 
 external print_string : string -> unit = "caml_numworks_print_string" [@@noalloc]
 external print_newline : unit -> unit = "caml_numworks_print_newline" [@@noalloc]
 external print_endline : string -> unit = "caml_numworks_print_endline" [@@noalloc]
 external print_int : int -> unit = "caml_numworks_print_int" [@@noalloc]
-(* external print_char : char -> unit = "caml_numworks_print_char" [@@noalloc] *)
-(* external print_float : float -> unit = "caml_numworks_print_float" [@@noalloc] *)
 
-(* EADK library *)
+let print_bool b =
+  if b = true then print_string "true"
+  else print_string "false"
+;;
+
+let print_char c = print_string (String.make 1 c);;
+let print_float f = print_string (string_of_float f);;
+
+
+(***********************************)
+(* Functions from the EADK library *)
+(***********************************)
+
 external display_draw_string : string -> int -> int -> unit = "caml_display_draw_string" [@@noalloc]
+external display_draw_string_small : string -> int -> int -> unit = "caml_display_draw_string_small" [@@noalloc]
+let display_draw_string_large = display_draw_string
+external display_draw_string_full : string -> int -> int -> bool -> int -> int -> unit = "caml_display_draw_string_full" [@@noalloc]
+let color_black : int = 0x0
+let color_white : int = 0xFFFF
+let color_red : int = 0xF800
+let color_green : int = 0x07E0
+let color_blue : int = 0x001F
+
+
+(*******************)
+(* Storage library *)
+(*******************)
+
+external read_any_file : string -> int = "caml_read_any_file" [@@noalloc]
+let read_ocaml_file () =
+  read_any_file "ocaml.py"
+;;
--- targets/numworks/numworks.mli
+++ targets/numworks/numworks.mli
@@ -1,18 +1,47 @@
+(***************************************)
+(** Signature of the Numworks library **)
+(***************************************)
+
+(**********)
 (* Timing *)
+(**********)
 
 val delay : int -> unit
 val delay_usec : int -> unit
 val millis : unit -> int
 
+
+(************)
 (* Printing *)
+(************)
 
 val print_string : string -> unit
 val print_newline : unit -> unit
 val print_endline : string -> unit
 val print_int : int -> unit
-(* val print_char : char -> unit *)
-(* val print_float : float -> unit *)
+val print_bool : bool -> unit
+val print_char : char -> unit
+val print_float : float -> unit
 
-(* EADK library *)
+
+(***********************************)
+(* Functions from the EADK library *)
+(***********************************)
+
 val display_draw_string : string -> int -> int -> unit
+val display_draw_string_small : string -> int -> int -> unit
+val display_draw_string_large : string -> int -> int -> unit
+val display_draw_string_full : string -> int -> int -> bool -> int -> int -> unit
+val color_black : int
+val color_white : int
+val color_red : int
+val color_green : int
+val color_blue : int
 
+
+(*******************)
+(* Storage library *)
+(*******************)
+
+val read_ocaml_file : unit -> int
+val read_any_file : string -> int
